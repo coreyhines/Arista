623a60300861fb415c97724c0efd0b6c1449ee03
Corey A Hines <chines@mbp-arista.lan>
2017-11-29 19:28:01 -0600

    cleanup

--- getVlanCounters.py
+++ getVlanCounters.py
@@ -1,27 +1,20 @@
 #!/usr/bin/env python
 
 '''
-
 This script relies on a few things
 1. you cave a .eapi.conf file in your ~/
-
     ** example .eapi.conf
     [connection:tg254]
     host: 172.28.129.56
     username: admin
     #password: (this test env did not need a password)
     transport: https
-
 2. You have enabled eapi on your switch: conf; management api https commands; no shut
 3. You have enabled vlan counters on the 7280R
-
     tg254.08:41:49(config)#hardware counter feature vlan in
     tg254.08:47:33(config)#hardware counter feature vlan out
-
 4. you have installed pyeapi 'pip install pyeapi'
-
 There is a lot of formatting and likely stripping away some of the output for your needs. Let me know if you need help with that.
-
 EXAMPLE OUTPUT:
 chines@mbp-arista-13122[~]$ ./getVlanCounters.py
 [{'command': 'show vlan counters',
@@ -58,14 +51,11 @@ chines@mbp-arista-13122[~]$ ./getVlanCounters.py
                                            u'outMcastPkts': 0,
                                            u'outOctets': 0,
                                            u'outUcastPkts': 0}}}}]
-
-
-
 '''
 
 
 import pyeapi
 from pprint import pprint as pp
 
-node = pyeapi.connect_to('tg254')
+node = pyeapi.connect_to('7150-01')
 pp(node.enable('show vlan counters'))
