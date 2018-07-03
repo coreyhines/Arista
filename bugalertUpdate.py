6668305ba179151fd846a57596c2ec5dcaa97584
Corey A Hines <chines@mbp-arista.lan>
2018-07-03 18:10:13 -0500

    This time for sure

--- bugalertUpdate.py
+++ bugalertUpdate.py
@@ -127,11 +127,12 @@ else:
     updatedDB = "NO"
     dbImported = "NO"
 
-print('SUMMARY').expandtabs(18)
+print('\nSUMMARY').expandtabs(18)
 print('\n' + 'alertDB' + '\t'+ 'Release Date' + '\t' + 'Version ID').expandtabs(18)
 print('----------' + '\t' + '------------' + '\t' + '------------------------------------').expandtabs(18)
 print('installed   -->' + '\t' + sysdb_releaseDate + '\t' + sysdb_version).expandtabs(18)
 print('available   -->' + '\t' + web_data['releaseDate'] + '\t' + web_version).expandtabs(18)
+print('\n')
 print('Database updated:' + '\t' +'\t' + updatedDB).expandtabs(18)
 print('Database imported:' + '\t' + dbImported).expandtabs(18)
 print('\n')
