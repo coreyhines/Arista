0929f7c58bc90303a8984285cde7e0d0205ee061
Corey A Hines <chines@mbp-arista.lan>
2018-07-03 23:26:20 -0500

    Changed install instructions added comments to main sections

--- bugalertUpdate.py
+++ bugalertUpdate.py
@@ -41,8 +41,9 @@ for database updates for the CVX Bugalerts feature.
 To learn more about BugAlerts see: https://eos.arista.com/eos-4-17-0f/bug-alerts/
 
 INSTALLATION
-1. Copy the script to /mnt/flash on CVX VM
-2. change values of username and password to a valid www.arista.com account
+1. ssh to the primary CVX node in the CVX cluster
+2. wget https://github.com/coreyhines/Arista/raw/master/bugalertUpdate.py 
+3. change values of username and password to a valid www.arista.com account
 3. run the script with.. ./bugalertUpdate.py
 """
 __author__ = 'chines'
@@ -58,7 +59,7 @@ password = 'CHANGEME'
 
 importdb = False
 
-stdout_ = sys.stdout            ### Trying to use this later to redirect output of the database import to cut the noise. This isn't working yet.
+stdout_ = sys.stdout            ### Trying to use this later to redirect output of the database import to cut the noise 
 stream = cStringIO.StringIO()
 
 string = username + ':' + password
@@ -74,31 +75,36 @@ result = requests.post(url, data=json.dumps(jsonpost))
 
 web_data = json.loads(result.text)
 web_data_final = result.text
+
 alertBaseFile = '/persist/sys/AlertBase.json'
 alertBaseFileFlash = '/mnt/flash/AlertBase.json'
 sysname = 'ar'
 
+# Instantiate an object form the class AlertBaseImporter
 alertBaseImporter = AlertBaseImporter( alertBaseFile, sysname )
 
 try:
     with open(alertBaseFile) as file:
-        pass
-except IOError as e:
+        pass # Check if the AlertBase JSON file exists
+except IOError as e: # handle the exception by downloading the AlertBase JSON
     print "Bug Alert Database does not exist. Downloading..."
     alertdbfile = open(alertBaseFile, 'w')
     alertdbfile.write(web_data_final)
     alertdbfile.close()
     copyfile(alertBaseFile, alertBaseFileFlash)
+    # Import the AlertBase JSON
     alertBaseImporter.loadAlertBase()
     print('\n\n Bug Alert Database successfully created and imported\n')
     exit(0)
 
+# Extract the BugAlert genId and the releaseDate from local CVX SysDB
 sysdb_version = alertBaseImporter.alertBaseSysdb.genId
 sysdb_releaseDate = alertBaseImporter.alertBaseSysdb.releaseDate
 
+# Extract the genId from the latest AlertBase available at arista.com
 web_version = web_data['genId']
 
-if  sysdb_version != web_version:
+if  sysdb_version != web_version: # If the version on arista.com is newer, download and install it
     print "\n Updating BugAlert database file!\n"
     alertdbfile = open(alertBaseFile, 'w')
     alertdbfile.write(web_data_final)
@@ -107,7 +113,6 @@ if  sysdb_version != web_version:
     updatedDB = "YES"
 else:
     importdb = False
-    #print('\n\n Bug Alert Database is already up to date!\n')
 
 if importdb == True:
     print('\n\n Bug Alert Database was updated, importing new entries...\n\n')
@@ -117,7 +122,6 @@ if importdb == True:
         sys.stdout = stdout_
         result = stream.getvalue()
         dbImported = "YES"
-        #print('\n\n Bug Alert Database successfully imported\n')
     except:
         sys.stdout = stdout_
         result = stream.getvalue()
@@ -128,6 +132,7 @@ else:
     updatedDB = "NO"
     dbImported = "NO"
 
+# Print a summary of what occurred in a table output
 print('\nSUMMARY').expandtabs(18)
 print('\n' + 'alertDB' + '\t'+ 'Release Date' + '\t' + 'Version ID').expandtabs(18)
 print('----------' + '\t' + '------------' + '\t' + '------------------------------------').expandtabs(18)
@@ -138,6 +143,8 @@ print('Database updated:' + '\t' +'\t' + updatedDB).expandtabs(18)
 print('Database imported:' + '\t' + dbImported).expandtabs(18)
 print('\n')
 
+# Failsafe to make sure the AlertBase file is in Flash as well as on /persist
+# This could be a case where file is deleted from /mnt/flash
 try:
     with open(alertBaseFileFlash) as file:
         pass
