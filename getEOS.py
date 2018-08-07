c04da2a38bb2a85a67266d9ed2d057dc0f732d9b
Corey Hines <chines@arista.com>
2018-08-06 19:06:41 -0500

    Initial commit of getEOS.py

---
+++ getEOS.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env Python
+
+import argparse
+import wget
+import sys
+import re
+import time
+from subprocess import Popen, call, check_output
+import os
+from os.path import expanduser
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+  "version", type=str, help="EOS image version", default=None)
+
+parser.add_argument(
+  "-p", "--package", type=str, default="EOS", choices=["EOS", "vEOS", "cEOS", "all"],
+  help="specify which EOS packaging", required=False)
+
+args = parser.parse_args()
+version = args.version
+
+images = []
+urls = []
+outputFilename = []
+
+if args.package == "vEOS":
+  images = ['vEOS-lab']
+elif args.package == "cEOS":
+  images = ['cEOS']
+elif args.package == "all":
+  images = ['EOS', 'cEOS', 'vEOS-lab']
+else:
+  images = ['EOS']
+
+if not version:
+  version = raw_input("Enter EOS Version: ").strip()
+
+home = expanduser("~")
+outputDir = home + "/Downloads/"
+
+for image in images:
+  if image == "cEOS":
+    ext = ".tar.xz"
+  else:
+    ext = ".swi"
+  urls.append("http://dist/release/EOS-" + version + "/final/images/" + image + ext)  
+  outputFilename.append(outputDir + image + "-" + version + ext)
+
+#print(urls)
+#outputFilename = outputDir + image + "-" + version + ".swi"
+
+
+vpncheckBin = check_output(["scutil", "--nc", "status", "Arista VPN"])
+vpncheckStr = vpncheckBin.split("\n")
+#print ("vpn check string: " + vpncheckStr[0])
+
+if vpncheckStr[0] == "Connected":
+  print("VPN Connection is up...\n")
+  try:
+    for url, filename in map(None, urls, outputFilename):
+      # print ("URL is " + url)
+      # print ("Filename is " + filename)
+      file = wget.download(url, filename)
+      print("\nFile downloaded to " + filename)
+    failed = False
+  except:
+    failed = True
+else:
+  print("VPN Connection is down...\n")
+  print("Attempting to connect to Arista VPN")
+  check_output(["scutil", "--nc", "start", "Arista VPN"])
+  while True:
+    vpncheckBin = check_output(["scutil", "--nc", "status", "Arista VPN"])
+    vpncheckStr = vpncheckBin.split("\n")
+    vpncheckResult = vpncheckStr[0].strip()
+    if vpncheckResult == "Connected":
+      break
+  #print ("Downloading EOS: " + images + " To: " + outputDir)
+  try:
+    for url, filename in map(None, urls, outputFilename):
+      # print ("URL is " + url)
+      # print ("Filename is " + filename)
+      file = wget.download(url, filename)
+      print("\nFile downloaded to " + filename)
+    failed = False
+  except:
+    failed = True
+
+if failed is False:
+  for filename, urls in map(None, outputFilename, urls):
+    filesize = os.stat(filename)[6]
+    if filesize < 1024:
+      with open(filename) as myfile:
+        content = myfile.read()
+      match = re.search(r'<p>(.*).</p>', content)
+      reason = match.group(1)
+      os.remove(filename)
+      print ("\n\nThere was an error downloading: " + url + "\n")
+      print ("\t" + reason + "\n")
