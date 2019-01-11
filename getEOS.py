5df42ffd4ce5db1f71eff9e33c057f00ea0c5f0d
Rob Martin <robmartin@arista.com>
2019-01-11 09:16:01 -0600

    fixed formatting

--- getEOS.py
+++ getEOS.py
@@ -79,10 +79,10 @@ def main(args,version):
       print ("Downloading EOS:" + version + " To: " + outputDir + "\n")
       try:
         for url, filename in map(None, urls, outputFilename):
-            # Adding in output to notify what file is currently being downloaded
-            print("\n[Starting] Download {}".format(filename.split('/')[::-1][0]))
-            file = wget.download(url, filename)
-            print("\n[Completed] File downloaded to " + filename)
+          # Adding in output to notify what file is currently being downloaded
+          print("\n[Starting] Download {}".format(filename.split('/')[::-1][0]))
+          file = wget.download(url, filename)
+          print("\n[Completed] File downloaded to " + filename)
         if args.releaseNotes:
           release_note = get_latest_rn(rn_url)
           if release_note:
