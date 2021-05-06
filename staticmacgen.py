daa4b3bf5022cdd8c30a6a8b9006e462e5d60737
coreyhines <chines@arista.com>
2021-05-06 05:06:11 +0000

    changes to password as an arg, user as an arg only, cleanups and try/except

--- staticmacgen.py
+++ staticmacgen.py
@@ -10,6 +10,9 @@ import random
 def main():
   parser = argparse.ArgumentParser()
   parser.add_argument(
+     "-u", "--user", type=str, default="",
+     help="specify a username", required=True)
+  parser.add_argument(
      "-s", "--session", type=str, default="",
      help="specify config session name", required=True)
   parser.add_argument(
@@ -19,8 +22,11 @@ def main():
       "-d", "--device", type=str, default="",
       help="specify an EOS device which static MACs are to applied", required=True)
   parser.add_argument(
-      "-u", "--undo", action='store_true',
-      help="specify --undo, to remove applied static mac addresses", required=False)
+      "-p", "--passwd", type=str, default="",
+      help="for passing password interactively", required=False)
+  parser.add_argument(
+      "-r", "--remove", action='store_true',
+      help="to remove applied static mac addresses", required=False)
   args = parser.parse_args()
   n = 4 
   rand = ''.join(random.choices(string.ascii_uppercase +
@@ -28,30 +34,37 @@ def main():
   session = args.session + "-" + rand
   file = args.file
   hostname = args.device
-  undo = args.undo
+  remove = args.remove
+  user = args.user
+
+  if len(args.passwd) > 0:
+    passwd = args.passwd
+  else:
+    passwd = '***REMOVED***'
 
   _create_unverified_https_context = ssl._create_unverified_context
   ssl._create_default_https_context = _create_unverified_https_context
 
-  user = 'admin'
-  passwd = '***REMOVED***'
-
-  current_file = open(file, "r")
-  macaddresses = current_file.readlines()
+  with open(file, "r") as current_file:
+    macaddresses = current_file.readlines()
 
   device = Server('https://{}:{}@{}/command-api'.format(user, passwd, hostname))
 
-  for mac in macaddresses:
-    #mac addresses in file should have format aa:aa:aa:aa:aa:aa.vlan#.interface#
-    bits = mac.split(".")
-    macaddr = bits[0]
-    vlan = bits[1]
-    intf = bits[2]
+  try:
+    for mac in macaddresses:
+      #mac addresses in file should have format aa:aa:aa:aa:aa:aa.vlan#.interface#
+      #
+      bits = mac.split(".")
+      macaddr = bits[0]
+      vlan = bits[1]
+      intf = bits[2]
     
-    if undo == True:
-      result = device.runCmds(1, ['enable', 'configure session {0}'.format(session), 'no mac address-table static {0}'.format(macaddr) + ' vlan {0}'.format(vlan) + ' interface {0}'.format(intf)])
-    else:
-      result = device.runCmds(1, ['enable', 'configure session {0}'.format(session), 'mac address-table static {0}'.format(macaddr) + ' vlan {0}'.format(vlan) + ' interface {0}'.format(intf)])
+      if remove == True:
+        result = device.runCmds(1, ['enable', 'configure session {0}'.format(session), 'no mac address-table static {0}'.format(macaddr) + ' vlan {0}'.format(vlan) + ' interface {0}'.format(intf)])
+      else:
+        result = device.runCmds(1, ['enable', 'configure session {0}'.format(session), 'mac address-table static {0}'.format(macaddr) + ' vlan {0}'.format(vlan) + ' interface {0}'.format(intf)])
+  except: 
+    print("something went wrong, check password\n\n")
 
   
   result2 = device.runCmds(1, ['enable', 'configure session {0}'.format(session) + ' commit'])
